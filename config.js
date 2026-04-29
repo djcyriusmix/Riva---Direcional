205e5f8dd9d87776ceb06162f3bd6fbb6952123a
djcyriusmix <djcyriusmix@gmail.com>
2026-04-29 15:44:03 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,29 @@
+// ARQUIVO DE CONFIGURAÇÃO DO SIMULADOR
+var CONFIG = {
+    // Configurações Visuais
+    "imagemFundo": "https://b3577058.smushcdn.com/3577058/wp-content/uploads/2025/05/Maiores-predios-do-Brasil-Orion-Complex-o-maior-predio-de-Goiania-720x511.webp?lossy=1&strip=0&webp=1",
+    "nomeConsultora": "JULIANE COSTA",
+
+    // Dados Básicos
+    "cliente": "",
+    "empreendimento": "",
+
+    // Painel Principal (Lado Esquerdo)
+    "vParcela": "R$ 800,00",
+    "vImovelLocal": "R$ 360.000,00",
+
+    // Valores e Descontos
+    "vFinanciado": "R$ 0,00",
+    "vDesconto": "R$ 0,00",
+    "vEntrada": "R$ 20.000,00",
+
+    // Subsídios e Facilitadores
+    "vMorarBem": "",
+    "vCodhab": "",
+    "vEntradaFacil": "30x de R$ 500,00",
+    "vObservacoes": "",
+
+    // Taxas e Encargos
+    "vCartorio": "Inclusa",
+    "vSeguroObra": "20% a 80%"
+};
